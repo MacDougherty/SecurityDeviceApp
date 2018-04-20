ad791d727d6ac53773d69f1f23f0dbd31463499f
Caroline Vanacore <vanacorec@gmail.com>
2018-04-20 15:54:57 -0400

    Modify Readme and apiotics.rb for generic hive

--- config/initializers/apiotics.rb
+++ config/initializers/apiotics.rb
@@ -1,7 +1,7 @@
 Apiotics.configure do |config|
   # The public and private keys are available on the Apiotics portal.  Navigate to your Hive's page and they will be towards the top
-  config.public_key = "6828078ec240b8d4114797ecceca7e0eeeb192c11dfd46df446e560f4a8bc82f" #set this to an Environment variable
-  config.private_key = "b5a9a4958ade1d81272a1311ab669f6954cdafefbc563b97de031f37a8bb2551" #set this to an Environment variable
+  config.public_key = nil #set this to an Environment variable
+  config.private_key = nil #set this to an Environment variable
   
   # If your local comms server is accepting TCP connections, the local port is the port that it will accept connections on
   config.local_port = 8001
